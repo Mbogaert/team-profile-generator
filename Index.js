7571d474422e02101ab4f825612c3963bca38391
Mathew B <bogaert@email.arizona.edu>
2020-12-04 00:46:48 -0700

    working on questions

--- Index.js
+++ Index.js
@@ -5,7 +5,9 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Employee = require('./lib/Employee');
 
-const promptUser = () => {
+function promptUser () {
+    let employees = [];
+    
     return inquirer.prompt([
         {
             type: 'input',
@@ -16,8 +18,24 @@ const promptUser = () => {
             type: 'input',
             name: 'id',
             message: 'Enter the employees id number'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter the employees email address'
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Select the employees role from the list',
+            choices: ['Manager', 'Engineer', 'Intern']
         }
     ])
+    .then(answers => {
+        employees.push(answers);
+        console.log(employees)
+    });
 }
 
-promptUser().then(answers => console.log(answers));
+
+promptUser()
